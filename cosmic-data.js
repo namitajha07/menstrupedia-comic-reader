bafaac8615d8ee67355235b112ed5f29ef1a2298
namitajha07 <namitajha668@gmail.com>
2026-07-13 15:28:48 +0530

    Create cosmic-data.js

---
+++ cosmic-data.js
@@ -0,0 +1,11 @@
+const COMIC_DATA = {
+    title: "Menstrupedia Comic",
+    chapters: [
+        {
+            title: "Chapter 1",
+            pages: [
+                "images/page1.jpg"
+            ]
+        }
+    ]
+};
